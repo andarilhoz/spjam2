36fad34bef821732b82076ac05e0575c2e6a860e
Magno Gouveia <owlflee@gmail.com>
2016-09-17 03:21:50 -0300

    Comentado o codigo, adicionado primeira versao da nave

--- Assets/Scritps/PlayerController.cs
+++ Assets/Scritps/PlayerController.cs
@@ -24,42 +24,41 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(1)) {
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        if (Input.GetMouseButton(1)) { //se pressionado botao direito do mouse
+            mousePos = Input.mousePosition; //pega posisao x e y do mouse
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos); 
 
-            Vector3 vectorToTarget = mousePos - transform.position;
-            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * turnSpeed);
+            Vector3 vectorToTarget = mousePos - transform.position; //distancia da nave pro mouse
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg; // angulo de diferença
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward); // Quartenion é o Objeto de transformação em graus
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * turnSpeed); // gire o objeto conforme o mouse na velocidade turnSpeed
 
-            if (actualSpeed < maxMoveSpeed && Vector3.Distance(transform.position, mousePos) > 10.5 ) {
-                actualSpeed += incrementMoveSpeed;
+            if (actualSpeed < maxMoveSpeed && Vector3.Distance(transform.position, mousePos) > 10.5 ) { // Se velocidade não chegou ao maximo e mouse está numa distancia maior que 10.06
+                actualSpeed += incrementMoveSpeed; // incrementa velocidade da nave
             }
 
 
             
-            transform.position = Vector2.Lerp(transform.position, mousePos, actualSpeed);
+            transform.position = Vector2.Lerp(transform.position, mousePos, actualSpeed); // atualiza posição da nave
 
-            float speed = (transform.position - LastPosition).magnitude / Time.deltaTime;
+            float speed = (transform.position - LastPosition).magnitude / Time.deltaTime; // pega velocidade real do objeto
             LastPosition = transform.position;
-
-            Debug.Log(Vector3.Distance(transform.position, mousePos));
-            if (actualSpeed > 0 && (speed <= 5 && Vector3.Distance(transform.position, mousePos) <= 10))
+            
+            if (actualSpeed > 0 && (speed <= 5 && Vector3.Distance(transform.position, mousePos) <= 10)) // caso clique e fique com o mouse parado no mesmo lugar diminui a velocidade ao chegar perto do mouse
             {
                 actualSpeed -= incrementMoveSpeed;
             }
-            else if (speed <= 10 && Vector3.Distance(transform.position, mousePos) <= 10.05)
+            else if (speed <= 10 && Vector3.Distance(transform.position, mousePos) <= 10.05) // se chegar muito perto do mouse zera a velocidade
                 actualSpeed = 0;
 
         }
-        else{
-            if (actualSpeed > 0) {
+        else{// caso não esteja clicnado com o mouse 
+            if (actualSpeed > 0) { // se velocidade atual for maior que zero
                 
-                actualSpeed -= incrementMoveSpeed;
-                transform.position = Vector2.Lerp(transform.position, mousePos, actualSpeed);
+                actualSpeed -= incrementMoveSpeed; //diminui velocidade aos poucos
+                transform.position = Vector2.Lerp(transform.position, mousePos, actualSpeed); // movimentação da nave
 
-                if (playerRig.velocity.x <= 0 && playerRig.velocity.y <= 0 && Vector3.Distance(transform.position, mousePos) <= 10)
+                if (playerRig.velocity.x <= 0 && playerRig.velocity.y <= 0 && Vector3.Distance(transform.position, mousePos) <= 10) // caso não esteja clicando com o mouse e nave esteja parada
                     actualSpeed = 0;
             }
         }
