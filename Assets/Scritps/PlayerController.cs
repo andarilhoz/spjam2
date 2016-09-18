180138b695b6614c5c7108c7cd87b3d9ff605689
Magno Gouveia <owlflee@gmail.com>
2016-09-18 09:47:24 -0300

    Animação inicial da nave feita

--- Assets/Scritps/PlayerController.cs
+++ Assets/Scritps/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour {
     private ParticleSystem.EmissionModule jatinhoemit;
     private ParticleSystem.EmissionModule atracaoemit;
     private ParticleSystem.EmissionModule repulsaoemit;
+    public bool animation = true;
 
     public ParticleSystem jatinho;
     public ParticleSystem atracao;
@@ -30,7 +31,8 @@ public class PlayerController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        //transform.position = new Vector3(-12, 0, 0);
+        transform.position = new Vector3(-12, 0, 0);
+        
         audio = GetComponent<AudioSource>();
         playerCollider = GetComponent<EdgeCollider2D>();
         playerRig = GetComponent<Rigidbody2D>();
@@ -48,21 +50,9 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-        #region Movimento Nave / Mouse
-        mousePos = Input.mousePosition; //pega posisao x e y do mouse
-        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        
-        Vector3 vectorToTarget = mousePos - transform.position; //distancia da nave pro mouse
-        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg; // angulo de diferença
-        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward); // Quartenion é o Objeto de transformação em graus
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * turnSpeed); // gire o objeto conforme o mouse na velocidade turnSpeed
-        
-        #endregion
+        if (animation)
+        {
 
-        #region Movimento/Botao direito
-
-        if (Input.GetMouseButton(1)) { //se pressionado botao direito do mouse
             if (!jatinho.isPlaying)
             {
                 jatinho.Simulate(0.0f, true, true);
@@ -70,57 +60,99 @@ public class PlayerController : MonoBehaviour {
                 jatinho.Play();
             }
 
-            Debug.Log(Vector3.Distance(transform.position, mousePos));
-            if (actualSpeed < maxMoveSpeed && Vector3.Distance(transform.position, mousePos) > 2.5 ) { // Se velocidade não chegou ao maximo e mouse está numa distancia maior que 10.06
-                actualSpeed += incrementMoveSpeed; // incrementa velocidade da nave
-            }
-            transform.position = Vector2.Lerp(transform.position, mousePos, actualSpeed); // atualiza posição da nave
-
-            speed = (transform.position - LastPosition).magnitude / Time.deltaTime; // pega velocidade real do objeto
-            LastPosition = transform.position;
-
-            if (actualSpeed > 0 && (speed <= 5 && Vector3.Distance(transform.position, mousePos) <= 10)) // caso clique e fique com o mouse parado no mesmo lugar diminui a velocidade ao chegar perto do mouse
+            transform.position = Vector2.Lerp(transform.position, new Vector3(-1, 0, 0), 0.01f);
+            if (transform.position.x >= -1.2)
             {
-                actualSpeed -= incrementMoveSpeed;
+                if (jatinho.isPlaying)
+                {
+                    jatinhoemit.enabled = false;
+                    jatinho.Stop();
+                }
+                animation = false;
             }
-            else if (speed <= 10 && Vector3.Distance(transform.position, mousePos) <= 4.05) // se chegar muito perto do mouse zera a velocidade
-                actualSpeed = 0;
 
         }
         else
-        {// caso não esteja clicnado com o mouse 
-            if (jatinho.isPlaying)
-            {
-                jatinhoemit.enabled = false;
-                jatinho.Stop();
-            }
-            if (actualSpeed > 0) { // se velocidade atual for maior que zero
-                
-                actualSpeed -= incrementMoveSpeed; //diminui velocidade aos poucos
-                transform.position = Vector2.Lerp(transform.position, mousePos, actualSpeed); // movimentação da nave
-
-                if (playerRig.velocity.x <= 0 && playerRig.velocity.y <= 0 && Vector3.Distance(transform.position, mousePos) <= 10) // caso não esteja clicando com o mouse e nave esteja parada
-                    actualSpeed = 0;
-            }
-            
-        }
-
-        #endregion
-
-
-        if (Input.GetMouseButton(0))
         {
-            onClickLeftMouseButton();
-        }
-        else {
-            if (atracao.isPlaying)
-            {
-                atracaoemit.enabled = false;
-                atracao.Stop();
+
+            #region Movimento Nave / Mouse
+            mousePos = Input.mousePosition; //pega posisao x e y do mouse
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            Vector3 vectorToTarget = mousePos - transform.position; //distancia da nave pro mouse
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg; // angulo de diferença
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward); // Quartenion é o Objeto de transformação em graus
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * turnSpeed); // gire o objeto conforme o mouse na velocidade turnSpeed
+
+            #endregion
+
+            #region Movimento/Botao direito
+
+            if (Input.GetMouseButton(1))
+            { //se pressionado botao direito do mouse
+                if (!jatinho.isPlaying)
+                {
+                    jatinho.Simulate(0.0f, true, true);
+                    jatinhoemit.enabled = true;
+                    jatinho.Play();
+                }
+
+                Debug.Log(Vector3.Distance(transform.position, mousePos));
+                if (actualSpeed < maxMoveSpeed && Vector3.Distance(transform.position, mousePos) > 2.5)
+                { // Se velocidade não chegou ao maximo e mouse está numa distancia maior que 10.06
+                    actualSpeed += incrementMoveSpeed; // incrementa velocidade da nave
+                }
+                transform.position = Vector2.Lerp(transform.position, mousePos, actualSpeed); // atualiza posição da nave
+
+                speed = (transform.position - LastPosition).magnitude / Time.deltaTime; // pega velocidade real do objeto
+                LastPosition = transform.position;
+
+                if (actualSpeed > 0 && (speed <= 5 && Vector3.Distance(transform.position, mousePos) <= 10)) // caso clique e fique com o mouse parado no mesmo lugar diminui a velocidade ao chegar perto do mouse
+                {
+                    actualSpeed -= incrementMoveSpeed;
+                }
+                else if (speed <= 10 && Vector3.Distance(transform.position, mousePos) <= 4.05) // se chegar muito perto do mouse zera a velocidade
+                    actualSpeed = 0;
+
             }
-            if (repulsao.isPlaying) {
-                repulsaoemit.enabled = false;
-                repulsao.Stop();
+            else
+            {// caso não esteja clicnado com o mouse 
+                if (jatinho.isPlaying)
+                {
+                    jatinhoemit.enabled = false;
+                    jatinho.Stop();
+                }
+                if (actualSpeed > 0)
+                { // se velocidade atual for maior que zero
+
+                    actualSpeed -= incrementMoveSpeed; //diminui velocidade aos poucos
+                    transform.position = Vector2.Lerp(transform.position, mousePos, actualSpeed); // movimentação da nave
+
+                    if (playerRig.velocity.x <= 0 && playerRig.velocity.y <= 0 && Vector3.Distance(transform.position, mousePos) <= 10) // caso não esteja clicando com o mouse e nave esteja parada
+                        actualSpeed = 0;
+                }
+
+            }
+
+            #endregion
+
+
+            if (Input.GetMouseButton(0))
+            {
+                onClickLeftMouseButton();
+            }
+            else
+            {
+                if (atracao.isPlaying)
+                {
+                    atracaoemit.enabled = false;
+                    atracao.Stop();
+                }
+                if (repulsao.isPlaying)
+                {
+                    repulsaoemit.enabled = false;
+                    repulsao.Stop();
+                }
             }
         }
 
