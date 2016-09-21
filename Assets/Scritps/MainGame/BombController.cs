c010c0c452072261890eb99dedac07f842f506c9
Magno Gouveia <owlflee@gmail.com>
2016-09-21 00:10:03 -0300

    Added particle and sound on bomb explode

--- Assets/Scritps/MainGame/BombController.cs
+++ Assets/Scritps/MainGame/BombController.cs
@@ -9,7 +9,6 @@ public class BombController : MonoBehaviour
 {
 
     public ParticleSystem explosion;
-
     public SpriteRenderer render;
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -17,12 +16,16 @@ public class BombController : MonoBehaviour
         
         if (collision.gameObject.tag == "spaceship" || collision.gameObject.tag == "barrier" )
         {
-            explosion.Play();
+            Explode(transform.position);
             //render.material.SetColor("_Color", new Color(render.material.color.r, render.material.color.g, render.material.color.b, 0f));
             //yield return new WaitForSeconds(1f);
             //explosion.Play();
             Destroy(gameObject);            
         }        
 
+    }
+    public void Explode(Vector3 position)
+    {
+        Instantiate(explosion, position, Quaternion.identity);
     }
 }
